5b1eab5f1de0a1208270565001a28d4652d32172
johnqing <csssnow@gmail.com>
2013-04-12 14:44:22 +0800

    修改

--- Ns.js
+++ Ns.js
@@ -1,113 +1,185 @@
 (function(global, undefined){
+    "use strict";
+    var _VERSION = '0.0.1',
+        _ua = navigator.userAgent.toLowerCase(),
+        _IE = _ua.indexOf('msie') > -1 && _ua.indexOf('opera') == -1,
+        _matches = /(?:msie|firefox|webkit|opera)[\/:\s](\d+)/.exec(_ua),
+        _V = _matches ? _matches[1] : '0',
+        doc = global.document,
+        body = doc.body,
+        rquickExpr = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/;
     //基础类
-    var Ns = function(selector, context){
-         //查询表达式必须为字符串，并且不能为空。
-        if(typeof(selector)!=='string' || selector == '') return [];
+    var Ns = function(expr, root){
+        return new Ns.fn.init(expr, root);
+    }
 
-        //使用空格分割，只处理第一个表达式
-        var ss = selector.split(' ');
+    /**
+     * 数组判断
+     * @param val
+     * @returns {boolean}
+     * @private
+     */
+    function _isArray(val) {
+    	if (!val) {
+    		return false;
+    	}
+    	return Object.prototype.toString.call(val) === '[object Array]';
+    }
 
-        //获取属性
-        var attr = '';
-        var s = ss[0].split(':')[0];
-        if(s != ss[0])
-            attr = ss[0].split(':')[1];
+    /**
+     * 函数判断
+     * @param val
+     * @returns {boolean}
+     * @private
+     */
+    function _isFunction(val) {
+    	if (!val) {
+    		return false;
+    	}
+    	return Object.prototype.toString.call(val) === '[object Function]';
+    }
+    function _inArray(val, arr) {
+    	for (var i = 0, len = arr.length; i < len; i++) {
+    		if (val === arr[i]) {
+    			return i;
+    		}
+    	}
+    	return -1;
+    }
+    function _each(obj, fn) {
+    	if (_isArray(obj)) {
+    		for (var i = 0, len = obj.length; i < len; i++) {
+    			if (fn.call(obj[i], i, obj[i]) === false) {
+    				break;
+    			}
+    		}
+    	} else {
+    		for (var key in obj) {
+    			if (obj.hasOwnProperty(key)) {
+    				if (fn.call(obj[key], key, obj[key]) === false) {
+    					break;
+    				}
+    			}
+    		}
+    	}
+    }
 
-        var val = s.split('[')[0];
-        if(val != s)
-            val = s.split('[')[1].replace(/[",\]]/g,'');
-        else
-            val = '';
-        s = s.split('[')[0];
-
-        var obj = [];
-        var sObj = null;
-        //当父对象不存在时，使用document;
-        context = context || document;
-        switch(s.charAt(0)){
-            case '#':
-                //ID选择器
-                sObj = document.getElementById(s.substr(1));
-                if(sObj)obj.push(sObj);
-                break;
-            case '.':
-                //类选择器
-                var l = context.getElementsByTagName('*');
-                var c = s.substr(1);
-                for(var i=0; i<l.length; i++)
-                if(l[i].className.search('\\b' + c + '\\b')!=-1)obj.push(l[i]);
-                break;
-            default:
-                //根据tag获取元素
-                obj = o.getElementsByTagName(s);
-                break;
+    /**
+     * 私有属性操作
+     * @param el
+     * @param val
+     * @returns {string}
+     * @private
+     */
+    function _getAttr(el, val){
+        return el.getAttribute(val);
+    }
+    function _setAttr(el, key, val){
+        if (_IE && _V < 8 && key.toLowerCase() == 'class') {
+            key = 'className';
         }
+        el.setAttribute(key, '' + val);
+    }
+    function _removeAttr(el, key) {
+    	if (_IE && _V < 8 && key.toLowerCase() == 'class') {
+    		key = 'className';
+    	}
+    	_setAttr(el, key, '');
+    	el.removeAttribute(key);
+    }
 
-        if(val){
-            //[t=val]筛选属性匹配
-            var l = [];
-            var a = val.split('=');
-            for(var i=0; i<obj.length; i++)
-                if(a.length == 2 && obj[i][a[0]] == a[1]) l.push(obj[i]);
-            obj = l;
-        }
-
-        if(attr){
-            //: 筛选属性匹配
-            var l = [];
-            for(var i=0; i<obj.length; i++)
-                if(obj[i][attr]) l.push(obj[i]);
-            obj = l;
-        }
-
-        if(ss.length > 1){
-            //递归处理表达式后续内容
-            //父元素为已获取的所有元素
-            var l = [];
-            for(var i=0; i<obj.length; i++){
-                var ll = arguments.callee(selector.substr(ss[0].length+1), obj[i]);
-                if(ll.tagName) l.push(ll);
-                else
-                for(var j=0; j<ll.length; j++)l.push(ll[j]);
-            }
-            obj = l;
-        }
-
-        if(sObj && ss.length == 1){
-            //当为ID选择器时，直接返回对象。
-            obj=sObj;
-            if(obj)obj.length = 1;
-        } else {
-            //去除数组中重复元素
-            var l = [];
-            for(var i=0; i<obj.length; i++)obj[i].$isAdd = false;
-            for(var i=0; i<obj.length; i++){
-                if(!obj[i].$isAdd){
-                    obj[i].$isAdd = true;
-                    l.push(obj[i]);
+    /**
+     * 是否含有class
+     * @private
+     */
+    function _hasClass(el, cls){
+        var result = _getElementsByClassName(el.parentNode, cls).length;
+        return (result > 0 ? true : false);
+    }
+    function _getElementsByClassName(el, cls){
+        if(el.getElementsByClassName){
+            return el.getElementsByClassName(cls);
+        }else{
+            var result = [];
+            var elems = el.getElementsByTagName('*');
+            for(var i = 0, len = elems.length; i < len; i++){
+                if(elems[i].className.indexOf(cls) != -1){
+                    result[result.length] = elems;
                 }
             }
-            obj = l;
+            return result;
         }
-        this.el = obj;
-        },
-        doc = global.document,
-        body = doc.body;
+    }
+    function _bind(elem, eventType, callback){
+        // 转为小写
+        eventType = eventType.toLowerCase();
+
+        if(elem.addEventListener){
+            elem.addEventListener(eventType, callback, false);
+        }else if(elem.attachEvent){
+            elem.attachEvent('on' + eventType, callback);
+        };
+    }
+    function _unbind(elem, eventType, callback){
+        // 转为小写
+        eventType = eventType.toLowerCase();
+
+        if ( elem.detachEvent ) {
+            elem.detachEvent("on" + eventType, callback);
+        } else{
+            elem.removeEventListener(elem, eventType, callback);
+        };
+    }
+    /**
+     * 向外传递接口
+     * @type {Function}
+     */
+    global.N = global.ns = Ns;
 
     //定义prototype别名
     Ns.fn = Ns.prototype = {
-        /*
-        *回调函数，用于处理所有方法中function类型的参数
-        *@param obj 任何类型
-        *@return 如果@param obj是一个函数类型的对象,则返回这个函数的返回值,如果是其他类型则直接返回@param obj
-        */
-        _preFormCallBack: function(obj){
-            var _this = this;
-            var ret = obj;
-            if(typeof(obj) == "function"){
-                ret = obj.call(_this.el,_this.el);
+        constructor: Ns,
+        init: function(expr, root){
+            var elem=[],
+                match,
+                m,
+                nodetype;
+            root = root || doc;
+            //N('');
+            if(!expr){
+                return this;
             }
-            return ret;
+            //html字符串检测
+            if(typeof expr === 'string'){
+                if(match = rquickExpr.exec(expr)){
+                    if(m = match[1]){//N(id);
+                        elem.push(root.getElementById( m ));
+                    }else if(match[2]){//N(tag);
+                        elem = root.getElementsByTagName( expr )
+                    }else if((m = match[3])){//$(class);
+                        elem = _getElementsByClassName(root, m);
+                    }
+                }
+            }else{
+                if(typeof expr === "object"){
+                    elem.push(expr);
+                }
+            }
+            this.expr = this[0] = elem;
+            return this;
+        },
+        /**
+         *
+         * @param fn
+         */
+        each: function(fn){
+            var self = this.expr;
+            for(var i = 0; i < self.length; i++){
+                if(fn.call(self[i], i, self[i]) == false){
+                    return self;
+                }
+            }
+            return this;
         },
         /**
          * Dom方法
@@ -115,22 +187,37 @@
          * @returns {*}
          */
         html: function(value){
-            if(value == undefined){
-                    return this.el.innerHTML;
-            }else{
-                    var v = this._preFormCallBack(v);
-                    this.el.innerHTML = value;
-                    return this;
+            var self = this.expr;
+            if(value === undefined){
+                if(self.length < 1){
+                    return '';
+                }
+                return self[0].innerHTML;
             }
+            this.each(function(){
+                this.innerHTML = value;
+            });
+            return this;
         },
-        attr: function(){
-            if(arguments.length == 1){
-                    return this.el.getAttribute(arguments[0]);
+        attr: function(key, val){
+            var self = this.expr;
+            if (key === undefined) {
+                return self;
             }
-            if(arguments.length == 2){
-                    this.el.setAttribute(arguments[0],arguments[1]);
-                    return this;
+            if (typeof key === 'object') {
+                _each(key, function(k, v) {
+                    self.attr(k, v);
+                });
+                return self;
             }
+            if (val === undefined) {
+                val = self.length < 1 ? null : _getAttr(self[0], key);
+                return val === null ? '' : val;
+            }
+            this.each(function() {
+                _setAttr(this, key, val);
+            });
+            return this;
         },
         /*
         *如果是无参调用 .hasClass()
@@ -141,48 +228,25 @@
         *@return 判断DOM element是否含有名为
         *@param v的className属性
         */
-        hasClass: function(v){
-            if(v == undefined){
-                    return this.el.className;
-            }else{
-                    if(this.el.className != undefined){
-                            var classStr = " " + this.el.className + " ";
-                            var rep = new RegExp(" " +v+ " ","gi");
-                            return rep.test(classStr);
-                    }else{
-                            return false;
-                    }
+        hasClass: function(cls){
+            var self = this.expr;
+            if(self.length < 1){
+                return false;
             }
+            return _hasClass(self[0], cls);
         },
         bind: function( eventType, callback){
-            elem = this.el || window;
-            // 转为小写
-            eventType = eventType.toLowerCase();
-
-            if(elem.addEventListener){
-                elem.addEventListener(eventType, callback, false);
-            }else if(elem.attachEvent){
-                elem.attachEvent('on' + eventType, callback);
-            };
+            this.each(function(){
+               _bind(this, eventType, callback);
+            });
         },
         unbind: function(eventType, callback){
-            elem = this.el;
-            eventType = eventType.toLowerCase();
-
-            if ( elem.detachEvent ) {
-                elem.detachEvent("on" + eventType, callback);
-            } else{
-                elem.removeEventListener(elem, eventType, callback);
-            };
+            this.each(function(){
+                _unbind(this, eventType, callback);
+            })
         }
     };
-    /**
-     * 向外传递接口
-     * @type {Function}
-     */
-    global.N = global.ns = function(selector, context){
-        return new Ns(selector, context);
-    }
+    Ns.fn.init.prototype = Ns.fn;
     /**
      * 拓展ns
      *如果是一个参数 .extend(obj)
@@ -194,7 +258,7 @@
      *
      *如果自定义的属性和_T中的属性重名 不会覆盖原有属性
      */
-    ns.extend = function(){
+    Ns.extend = Ns.fn.extend = function(){
         if(arguments.length == 1){
                 for(var i in arguments[0]){
                         Ns.prototype[i] = arguments[0][i];
